80cdbc48a115d41114cfbf2086eaa072ddb99240
2sojeong <118418288+2sojeong@users.noreply.github.com>
2023-04-01 01:39:17 +0900

    update component 1 & 8

--- src/pages/InfoPage2.js
+++ src/pages/InfoPage2.js
@@ -1,10 +1,12 @@
 import Component8 from '../components/component8';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Component10 from '../components/component10';
 const InfoPage2 = () => {
     return <>
         {/* <Header /> */}
         <Component8 />
+        {/* <Component10/> */}
         <Footer />
     </>
 }
