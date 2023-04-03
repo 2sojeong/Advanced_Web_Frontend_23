e3d0ace75fbf3b0bb8c30afa6f45e3fd503a3d8e
bamb14 <althcjstk08@gachon.ac.kr>
2023-04-03 11:45:09 +0900

    update com3

--- src/pages/InfoPage1.js
+++ src/pages/InfoPage1.js
@@ -1,6 +1,7 @@
 import Component1 from '../components/component1';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Component3 from '../components/component3';
 const InfoPage1 = () => {
     return <>
         <Header />
