2b1b69f15e5cca5e114c92f34857b2851de8fa14
eilsamo600 <eilsamo600@gachon.ac.kr>
2023-04-04 13:58:46 +0900

    Update component12

---
+++ src/components/component2.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Component2 = () => {
+    return (
+        <>
+            <div >
+                <section>
+                    <div>
+                        <figure>
+                            <picture>
+                                <source media="(min-width: 1025px)" srcset="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" />
+                                <img loading="lazy" src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" width="1440" height="380" alt="" data-pc-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" data-pc-width="1440" data-pc-height="380" data-tablet-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-tab-res.png?$ORIGIN_PNG$" data-tablet-width="1023" data-tablet-height="300" data-mobile-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/Home_Careers_banner_MO.png?$ORIGIN_PNG$" data-mobile-width="767" data-mobile-height="240"></img>
+                            </picture>
+                        </figure>
+                        <div>
+                            <div>채용</div>
+                            <div>"함께 지식을 쌓고" <br /> "성장합니다."</div>
+                            <div><a href="/kr/careers/" class="Home_career-banner__item-btn dark-bg btn-large highContrastTarget" data-an-tr="body contents" data-an-ca="link click" data-an-ac="더 알아보기" data-an-la="career banner">더 알아보기</a></div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        </>
+    );
+}
+
+export default Component2;
--- src/pages/InfoPage1.js
+++ src/pages/InfoPage1.js
@@ -1,5 +1,10 @@
 import Component1 from '../components/component1';
+<<<<<<< Updated upstream
 import Component4 from '../components/component4';
+=======
+import Component2 from '../components/component2';
+import Component12 from '../components/component12';
+>>>>>>> Stashed changes
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Component3 from '../components/component3';
@@ -7,8 +12,13 @@ const InfoPage1 = () => {
     return <>
         <Header />
         <Component1 />
+<<<<<<< Updated upstream
         <Component3 />
         <Component4 />
+=======
+        <Component2 />
+        <Component12 />
+>>>>>>> Stashed changes
         <Footer />
     </>
 }
