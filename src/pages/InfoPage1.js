6353dcc3e47b9a7a72ccb0dccf397bf73ac7229a
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-04-04 19:02:20 +0900

    remove conflict code

--- src/pages/InfoPage1.js
+++ src/pages/InfoPage1.js
@@ -1,26 +1,20 @@
 import Component1 from '../components/component1';
-<<<<<<< Updated upstream
 import Component4 from '../components/component4';
-=======
 import Component2 from '../components/component2';
 import Component12 from '../components/component12';
->>>>>>> Stashed changes
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Component3 from '../components/component3';
 const InfoPage1 = () => {
-    return <>
-        <Header />
-        <Component1 />
-<<<<<<< Updated upstream
-        <Component3 />
-        <Component4 />
-=======
-        <Component2 />
-        <Component12 />
->>>>>>> Stashed changes
-        <Footer />
-    </>
+        return <>
+                <Header />
+                <Component1 />
+                <Component3 />
+                <Component4 />
+                <Component2 />
+                <Component12 />
+                <Footer />
+        </>
 }
 
 export default InfoPage1;
