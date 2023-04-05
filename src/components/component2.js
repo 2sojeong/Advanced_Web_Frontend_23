91ed2cc6de1ca0b530c6dfe525edf82f043f175e
eilsamo600 <eilsamo600@gachon.ac.kr>
2023-04-06 03:56:02 +0900

    Update component2.js

--- src/components/component2.js
+++ src/components/component2.js
@@ -3,22 +3,34 @@ import React from "react";
 const Component2 = () => {
     return (
         <>
-            <div >
-                <section>
-                    <div>
-                        <figure>
-                            <picture>
-                                <source media="(min-width: 1025px)" srcset="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" />
-                                <img loading="lazy" src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" width="1440" height="380" alt="" data-pc-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" data-pc-width="1440" data-pc-height="380" data-tablet-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-tab-res.png?$ORIGIN_PNG$" data-tablet-width="1023" data-tablet-height="300" data-mobile-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/Home_Careers_banner_MO.png?$ORIGIN_PNG$" data-mobile-width="767" data-mobile-height="240"></img>
-                            </picture>
-                        </figure>
-                        <div>
-                            <div>채용</div>
-                            <div>"함께 지식을 쌓고" <br /> "성장합니다."</div>
-                            <div><a href="/kr/careers/" class="Home_career-banner__item-btn dark-bg btn-large highContrastTarget" data-an-tr="body contents" data-an-ca="link click" data-an-ac="더 알아보기" data-an-la="career banner">더 알아보기</a></div>
-                        </div>
+            <div style={{ maxWidth: '100%', width: '100%', position: 'relative', margin: '0 auto', border: '0', display: 'block' }} class="Home_career-banner">
+                <figure style={{ isolation: 'isolate', display: 'block', padding: '0px', margin: '0px' }} class="Home_career-banner__thum">
+                    <picture style={{ width: '100%', textAlign: 'center', wordBreak: 'keep-all', display: 'block', margin: '0', padding: '0' }}>
+
+                        <source media="(min-width: 1025px)" srcset="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" />
+
+                        <source media="(min-width: 768px) and (max-width: 1024px)" srcset="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-tab-res.png?$ORIGIN_PNG$" />
+
+
+                        <source media="(min-width: 0px) and (max-width: 767px)" srcset="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/Home_Careers_banner_MO.png?$ORIGIN_PNG$" />
+
+
+
+                        <img style={{ height: '400px', width: '100%', maxHeight: '570px', maxWidth: '100%', objectFit: 'cover', verticalAlign: 'top' }} loading="lazy" src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" width="1440" height="380" alt="" data-pc-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-pc-res2.png?$ORIGIN_PNG$" data-pc-width="1440" data-pc-height="380" data-tablet-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/th-build-your-knowledge-tab-res.png?$ORIGIN_PNG$" data-tablet-width="1023" data-tablet-height="300" data-mobile-src="https://image.semiconductor.samsung.com/image/samsung/p6/semiconductor/home/Home_Careers_banner_MO.png?$ORIGIN_PNG$" data-mobile-width="767" data-mobile-height="240" />
+
+                    </picture>
+                </figure>
+                <div style={{ position: 'absolute', padding: '0 200px', top: 'calc(50% - 80px)', color: '#ffffff' }} class="Home_career-banner__text">
+                    <div style={{ fontSize: '30px', fontWeight: '700' }} class="Home_career-banner__title">채용</div>
+                    <div style={{ fontSize: '35px', fontWeight: '700' }} class="Home_career-banner__description highContrastTarget">
+                        함께 지식을 쌓고<br />
+                        성장합니다.
                     </div>
-                </section>
+
+                    <div class="Home_career-banner__item-btn-wrap">
+                        <a style={{ backgroundColor: '#ffffff', color: '#000000' }} href="/kr/careers/" class="Home_career-banner__item-btn dark-bg btn-large highContrastTarget" data-an-tr="body contents" data-an-ca="link click" data-an-ac="더 알아보기" data-an-la="career banner">더 알아보기</a>
+                    </div>
+                </div>
             </div>
 
         </>
