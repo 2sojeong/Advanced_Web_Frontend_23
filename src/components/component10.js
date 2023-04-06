2f950d835f21ab585c7e477882ed39760ec7872d
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-04-06 13:39:01 +0900

    update resizing com10,5

--- src/components/component10.js
+++ src/components/component10.js
@@ -7,10 +7,10 @@ const Component10 = (props) => {
     if (reverse === undefined) {
         reverse = false
     }
-    const Img = () => <img style={{ width: '350px' }} src={bannerImg}></img>
-    const Content = () => <div style={{ margin: 'auto 0', marginLeft: reverse ? '0px' : '200px', marginRight: reverse ? '200px' : '0px' }}>
-        <p style={{ fontWeight: 'bold', fontSize: '30px', margin: '0px 0px 10px 0px' }}>{props.head}</p>
-        <p style={{ fontWeight: 'lighter', lineHeight: '25px' }}>{props.contents.map((content) => <>{content}<br /></>)}</p>
+    const Img = () => <img style={{ width: '550px' }} src={bannerImg}></img>
+    const Content = () => <div style={{ margin: 'auto 0', marginLeft: reverse ? '0px' : '330px', marginRight: reverse ? '330px' : '0px' }}>
+        <p style={{ fontWeight: 'bold', fontSize: '37px', margin: '0px 0px 10px 0px' }}>{props.head}</p>
+        <p style={{ fontWeight: 'lighter', lineHeight: '30px', fontSize: '19px' }}>{props.contents.map((content) => <>{content}<br /></>)}</p>
     </div>
     return (
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center', margin: '75px auto' }}>
--- src/components/component5.js
+++ src/components/component5.js
@@ -7,32 +7,32 @@ const Component5 = () => {
             <div style={{
                 display: 'flex',
                 justifyContent: 'center',
-                maxWidth: '1190px', margin: '50px auto', position: 'relative'
+                maxWidth: '1300px', margin: '50px auto', position: 'relative'
             }}>
 
                 <h2 style={{
                     margin: '0',
-                    paddingRight: '160px',
+                    paddingRight: '400px',
                     fontSize: '35px',
                     fontWeight: '600',
                     lineHeight: '1.18'
                 }}>Manifesto</h2>
-                <div style={{ lineHeight: '28px' }} >
-                    <b style={{ fontSize: '19px' }}>
+                <div style={{ lineHeight: '30px' }} >
+                    <b style={{ fontSize: '21px' }}>
                         우리는 꿈을 현실로 만드는 여정에 함께하는 동반자들이다.
                         <br />
                         우리는 사람들이 자신의 꿈을 실현하는 것을 도움으로써
                         <br />
                         더 나은 세상을 만드는데 기여한다.
                     </b>
-                    <p style={{ fontSize: '16px', marginTop: '30px' }}>
+                    <p style={{ fontSize: '19px', marginTop: '30px' }}>
                         기술과 서비스,
                         <br />
                         그리고 혁신적인 아이디어로 우리 모두의 꿈을 현실로 만듭니다.
                     </p>
                 </div>
             </div>
-            <img src={bannerImg} style={{ margin: '60px auto', display: 'block', width: "900px", borderRadius: '18px' }}></img>
+            <img src={bannerImg} style={{ margin: '60px auto', display: 'block', width: "1100px", borderRadius: '18px' }}></img>
 
         </>
     );
