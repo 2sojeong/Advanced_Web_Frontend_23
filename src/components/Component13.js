4703078eee8f73a036f5fa275e5c26595e49d021
bamb14 <althcjstk08@gachon.ac.kr>
2023-04-06 16:41:34 +0900

    Update Component13.js

--- src/components/Component13.js
+++ src/components/Component13.js
@@ -13,7 +13,7 @@ const Component13 = () => {
                     paddingBottom: "10px",
                     fontSize: "100px",
                     lineHeight: "70px",
-                    fontWeight: 500,
+                    fontWeight: "500",
                     letterSpacing: "-3px"
                 }}>Biologics<br />
                     <span style={{
@@ -25,9 +25,16 @@ const Component13 = () => {
                     wordBreak: "keep-all"
                 }}></p>
                 <p style={{ fontSize: "18px" }}>셀트리온의 항체 바이오시밀러는 환자들에게 합리적 비용으로</p>
-                <p style={{ fontSize: "18px" }}>고품질 바이오의약품 치료를 받을 수 있는 기회를 제공합니다</p>
+                <p style={{ lineHeight: "7px", fontSize: "18px" }}>고품질 바이오의약품 치료를 받을 수 있는 기회를 제공합니다</p>
                 <p></p>
-                <p></p>
+                <p style={{ marginTop: "50px" }}>
+                    <a style={{ fontSize: "14px" }} href="https://www.celltrion.com/ko-kr/Business/Self">Learn more
+                        <img style={{
+                            verticalAlign: "middle",
+                            paddingLeft: "10px"
+                        }} src="https://www.celltrion.com/image/common/icon_btn_more.png" alt />
+                    </a>
+                </p>
             </div>
 
             <img style={{ width: "50%", display: 'block' }} src="https://cellhomeblob.blob.core.windows.net/bns/191713570956-c6c1b49b-a1e9-43c1-9a95-34a6db491d93.jpg"></img>
